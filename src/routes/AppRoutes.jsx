bd4e525c50ea8049f32801b4bfdc0bc9110896e4
kamran sidiq <kamransidiq169@gmail.com>
2025-10-19 23:06:30 +0530

    third change

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,7 +5,7 @@ import UserRegister from "../pages/UserRegister";
 import UserLogin from "../pages/UserLogin";
 import FoodPartnerRegister from "../pages/FoodPartnerRegister";
 import FoodPartnerLogin from "../pages/FoodPartnerLogin";
-import Home from "../pages/home";
+import Home from "../pages/Home.jsx";
 import { CreateFood } from "../pages/CreateFood";
 import  Profile  from "../pages/Profile";
 import { FirstView } from "../FirstView";
